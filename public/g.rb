efe17d022588edd24b842881032e6620bea41f68
cory mcdonald <cory@corywmcdonald.com>
2018-11-30 15:34:03 -0600

    done

---
+++ public/g.rb
@@ -0,0 +1,12 @@
+require 'fileutils'
+
+folders = Dir.entries('.').select {|entry| File.directory? entry}.map{ |x| x.to_i}.uniq.sort
+
+new_folder = folders.last + 1
+
+Dir.mkdir new_folder.to_s
+FileUtils.cp('template.html', "#{new_folder}/index.html")
+FileUtils.cp('template.css', "#{new_folder}/index.css")
+
+
+`code #{new_folder}/index.html`
