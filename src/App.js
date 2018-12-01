c08b7eedf72e869ae9ea8b37809e93b4f2dadd10
cory mcdonald <cory@corywmcdonald.com>
2018-11-30 20:00:28 -0600

    Update website with links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import React, { Component, Fragment } from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import React, {Component, Fragment} from 'react';
 import runningman from './runningman.gif';
 import './App.css';
 import Options from './Options';
@@ -8,48 +8,64 @@ import Advertise from './Advertise';
 
 class App extends Component {
   constructor (props) {
-    super(props);
+    super (props);
     this.state = {
-      yo: "yo"
-    }
+      yo: 'yo',
+    };
   }
   activateLasers = () => {
-    this.setState({yo: this.state.yo + "o"})
+    this.setState ({yo: this.state.yo + 'o'});
+  };
 
-  }
 
-  render() {
+  render () {
+
+    var links =[]
+    for (var i =0; i<= 28; i++)  {
+      links.push({ key: i, href: `/${i}`})
+    }
+
     return (
+
       <div className="App home">
-      <div id="organize">
-       <img src={runningman} width="800px" height="500px"></img>
-         <header>
-          <h1>error for oh for</h1>
-        </header>
-      </div>
-      <div >
-<a href="/options" id="homelink">There's no website here</a>
-      </div>
+        <div class="list">
+        <h2>Websites created</h2>
+        {links.map(l => (
+          <div><a href={l.href} >Website {l.key}</a></div>
+        ))}
+
+
+        </div>
+
+        <div id="organize">
+          <img src={runningman} width="800px" height="500px" />
+          <header>
+            <h1>error for oh for</h1>
+          </header>
+        </div>
+        <div>
+          <a href="/options" id="homelink">There's no website here</a>
+        </div>
+
       </div>
     );
   }
 }
-
 
 const Index = () => <h2>Home</h2>;
 const About = () => <h2>About</h2>;
 const Users = () => <h2>Users</h2>;
 
 const AppRouter = () => (
-<Router> 
-  <Fragment>
-    <Route path="/" exact component={App} />
-    <Route path="/options/" component={Options} />
-    
-    <Route path="/Generator/" component={Generator} />
-    <Route path="/Advertise/" component={Advertise} />
+  <Router>
+    <Fragment>
+      <Route path="/" exact component={App} />
+      <Route path="/options/" component={Options} />
+
+      <Route path="/Generator/" component={Generator} />
+      <Route path="/Advertise/" component={Advertise} />
     </Fragment>
-    </Router>
+  </Router>
 );
 
 export default AppRouter;
